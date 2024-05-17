7eff80a1a3d47976525a74e1c6bc07ff402f8593
kimihito <tatsurotamashiro@gmail.com>
2024-05-17 17:31:08 +0900

    hex new phoenix_htmx_plug

---
+++ lib/phoenix_htmx_plug.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixHtmxPlug do
+  @moduledoc """
+  Documentation for `PhoenixHtmxPlug`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixHtmxPlug.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_htmx_plug_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixHtmxPlugTest do
+  use ExUnit.Case
+  doctest PhoenixHtmxPlug
+
+  test "greets the world" do
+    assert PhoenixHtmxPlug.hello() == :world
+  end
+end
